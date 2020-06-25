1c2581416f966970e3e5aa8bf72dd92b3e894821
rori4 <rangelstoilov@gmail.com>
2020-06-25 21:00:00 +0300

    revert to simple-oauth2 1.2

--- index.js
+++ index.js
@@ -1,79 +1,77 @@
-require("dotenv").config({ silent: true });
-const express = require("express");
-const { AuthorizationCode } = require("simple-oauth2");
-const randomstring = require("randomstring");
-const port = process.env.PORT || 3000;
-const oauthProvider = process.env.OAUTH_PROVIDER || "github";
-const loginAuthTarget = process.env.AUTH_TARGET || "_self";
+require('dotenv').config({silent: true})
+const express = require('express')
+const simpleOauthModule = require('simple-oauth2')
+const randomstring = require('randomstring')
+const port = process.env.PORT || 3000
+const oauthProvider = process.env.OAUTH_PROVIDER || 'github'
+const loginAuthTarget = process.env.AUTH_TARGET || '_self'
 
-const app = express();
-const config = {
+const app = express()
+const oauth2 = simpleOauthModule.create({
   client: {
     id: process.env.OAUTH_CLIENT_ID,
-    secret: process.env.OAUTH_CLIENT_SECRET,
+    secret: process.env.OAUTH_CLIENT_SECRET
   },
   auth: {
     // Supply GIT_HOSTNAME for enterprise github installs.
-    tokenHost: process.env.GIT_HOSTNAME || "https://github.com",
-    tokenPath: process.env.OAUTH_TOKEN_PATH || "/login/oauth/access_token",
-    authorizePath: process.env.OAUTH_AUTHORIZE_PATH || "/login/oauth/authorize",
-  },
-};
+    tokenHost: process.env.GIT_HOSTNAME || 'https://github.com',
+    tokenPath: process.env.OAUTH_TOKEN_PATH || '/login/oauth/access_token',
+    authorizePath: process.env.OAUTH_AUTHORIZE_PATH || '/login/oauth/authorize'
+  }
+})
 
-const originPattern = process.env.ORIGIN || "";
-if ("".match(originPattern)) {
-  console.warn(
-    "Insecure ORIGIN pattern used. This can give unauthorized users access to your repository."
-  );
-  if (process.env.NODE_ENV === "production") {
-    console.error("Will not run without a safe ORIGIN pattern in production.");
-    process.exit();
+const originPattern = process.env.ORIGIN || ''
+if (('').match(originPattern)) {
+  console.warn('Insecure ORIGIN pattern used. This can give unauthorized users access to your repository.')
+  if (process.env.NODE_ENV === 'production') {
+    console.error('Will not run without a safe ORIGIN pattern in production.')
+    process.exit()
   }
 }
 
-const client = new AuthorizationCode(config);
 // Authorization uri definition
-const authorizationUri = client.authorizeURL({
+const authorizationUri = oauth2.authorizationCode.authorizeURL({
   redirect_uri: process.env.REDIRECT_URL,
-  scope: process.env.SCOPES || "repo,user",
-  state: randomstring.generate(32),
-});
+  scope: process.env.SCOPES || 'repo,user',
+  state: randomstring.generate(32)
+})
 
 // Initial page redirecting to Github
-app.get("/auth", (req, res) => {
-  res.redirect(authorizationUri);
-});
+app.get('/auth', (req, res) => {
+  res.redirect(authorizationUri)
+})
 
 // Callback service parsing the authorization token and asking for the access token
-app.get("/callback", async (req, res) => {
-  const code = req.query.code;
-  const options = {
-    code: code,
-  };
-
-  if (oauthProvider === "gitlab") {
-    options.client_id = process.env.OAUTH_CLIENT_ID;
-    options.client_secret = process.env.OAUTH_CLIENT_SECRET;
-    options.grant_type = "authorization_code";
-    options.redirect_uri = process.env.REDIRECT_URL;
+app.get('/callback', (req, res) => {
+  const code = req.query.code
+  var options = {
+    code: code
   }
 
-  let mess, content;
-  try {
-    const token = await client.getToken(options);
-    mess = "success";
-    console.log(result);
-    content = {
-      token: token,
-      provider: oauthProvider,
-    };
-  } catch (error) {
-    console.error("Access Token Error", error.message);
-    mess = "error";
-    content = JSON.stringify(error);
+  if (oauthProvider === 'gitlab') {
+    options.client_id = process.env.OAUTH_CLIENT_ID
+    options.client_secret = process.env.OAUTH_CLIENT_SECRET
+    options.grant_type = 'authorization_code'
+    options.redirect_uri = process.env.REDIRECT_URL
   }
 
-  const script = `
+  oauth2.authorizationCode.getToken(options, (error, result) => {
+    let mess, content
+
+    if (error) {
+      console.error('Access Token Error', error.message)
+      mess = 'error'
+      content = JSON.stringify(error)
+    } else {
+      const token = oauth2.accessToken.create(result)
+      mess = 'success'
+      content = {
+        token: token.token.access_token,
+        provider: oauthProvider
+      }
+    }
+
+    const script = `
     <script>
     (function() {
       function recieveMessage(e) {
@@ -93,21 +91,22 @@ app.get("/callback", async (req, res) => {
       console.log("Sending message: %o", "${oauthProvider}")
       window.opener.postMessage("authorizing:${oauthProvider}", "*")
     })()
-    </script>`;
-  return res.send(script);
-});
+    </script>`
+    return res.send(script)
+  })
+})
 
-app.get("/success", (req, res) => {
-  res.send("");
-});
+app.get('/success', (req, res) => {
+  res.send('')
+})
 
-app.get("/", (req, res) => {
+app.get('/', (req, res) => {
   res.send(`Hello<br>
     <a href="/auth" target="${loginAuthTarget}">
       Log in with ${oauthProvider.toUpperCase()}
-    </a>`);
-});
+    </a>`)
+})
 
 app.listen(port, () => {
-  console.log("gandalf is walkin' on port " + port);
-});
+  console.log("gandalf is walkin' on port " + port)
+})
